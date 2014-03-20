27401c6143317390f80df91ef3d8ca1d730a0452
Jon Petto <jonpetto@gmail.com>
2014-03-20 11:59:40 -0500

    Add firefox desktop pages. Bug 975156.

---
+++ media/js/firefox/desktop/speed-graph.js
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+(function($) {
+    'use strict';
+
+    var $speed_graph = $('#speed-graph');
+
+    $speed_graph.closest('.container').waypoint(function() {
+        $speed_graph.addClass('animate');
+    }, {
+        triggerOnce: true,
+        offset: 100
+    });
+})(window.jQuery);
